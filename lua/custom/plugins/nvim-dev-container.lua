2c003d0faf39d20ae6101f6b09a4b78350e82275
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2024-01-29 08:07:22 +0000

    Update dev container config

--- lua/custom/plugins/nvim-dev-container.lua
+++ lua/custom/plugins/nvim-dev-container.lua
@@ -3,6 +3,22 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
     -- Run with the default configuration
-    require("devcontainer").setup{}
+    require("devcontainer").setup {
+      attach_mounts = {
+        always = true,
+        neovim_config = {
+          enabled = true,
+          options = { "readonly" },
+        },
+        neovim_data = {
+          enabled = false,
+          options = {},
+        },
+        neovim_state = {
+          enabled = false,
+          options = {},
+        },
+      },
+    }
   end,
 }
